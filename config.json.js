a8ba2fce71e51b241289cb49600539a32f1b6460
Magic-Xin <x.yu2000@163.com>
2019-07-20 01:13:20 +0800

    3.0.5

--- config.json.js
+++ config.json.js
@@ -1,2 +1,10 @@
 {
+"build_date":"2019-07-20_01:11:58",
+"description":"shadowsocks",
+"home_url":"Main_Ss_Content.asp",
+"md5":"928294866e8a7a69fbec6adcec31467e",
+"name":"shadowsocks",
+"tar_url": "https://github.com/Magic-Xin/ssr_mips/blob/master/shadowsocks.tar.gz", 
+"title":"shadowsocks",
+"version":"3.0.5"
 }
