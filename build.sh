63eb46d4c444657462099e3b6b191f68dbf652a5
MagicXin <x.yu2000@163.com>
2019-07-20 00:28:13 +0800

    3.0.5

--- build.sh
+++ build.sh
@@ -1,30 +1,41 @@
 #!/bin/sh
 
-
 MODULE=shadowsocks
-VERSION=3.0.4
+VERSION=`cat ./shadowsocks/ss/version|sed -n 1p`
 TITLE=shadowsocks
 DESCRIPTION=shadowsocks
 HOME_URL=Main_Ss_Content.asp
 
-# Check and include base
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-if [ "$MODULE" == "" ]; then
-	echo "module not found"
-	exit 1
-fi
+do_build() {
+	if [ "$VERSION" = "" ]; then
+		echo "version not found"
+		exit 3
+	fi
+	
+	rm -f ${MODULE}.tar.gz
+	rm -f $MODULE/.DS_Store
+	rm -f $MODULE/*/.DS_Store
+	tar -zcvf ${MODULE}.tar.gz $MODULE
+	md5value=`md5sum ${MODULE}.tar.gz|tr " " "\n"|sed -n 1p`
+	cat > ./version <<-EOF
+	$VERSION
+	$md5value
+	EOF
+	cat version
+	
+	DATE=`date +%Y-%m-%d_%H:%M:%S`
+	cat > ./config.json.js <<-EOF
+	{
+	"build_date":"$DATE",
+	"description":"$DESCRIPTION",
+	"home_url":"$HOME_URL",
+	"md5":"$md5value",
+	"name":"$MODULE",
+	"tar_url": "https://github.com/Magic-Xin/ssr_mips/blob/master/shadowsocks.tar.gz", 
+	"title":"$TITLE",
+	"version":"$VERSION"
+	}
+	EOF
+}
 
-if [ -f "$DIR/$MODULE/$MODULE/install.sh" ]; then
-	echo "install script not found"
-	exit 2
-fi
-
-# now include build_base.sh
-. $DIR/../softcenter/build_base.sh
-
-# change to module directory
-cd $DIR
-
-# do something here
-
-do_build_result
+do_build
